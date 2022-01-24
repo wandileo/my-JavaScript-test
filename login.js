e695063db66f0631527714f55c2c9fa09d38a378
unknown <wandilemaluleke@gmail.com>
2022-01-24 16:38:56 +0200

    submit buttons added

--- login.js
+++ login.js
@@ -2,10 +2,5 @@ let names = [];
 let password = [];
 
 function addLogin() {
-    var names = document.getElementById("names");
-
-    if (names == "") {
         alert("Email or Password is incorrect");
-        return;
-    }
 }
