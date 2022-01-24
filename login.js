7a663aae1eebd1342eebdec266dd4c6ee9c4b537
unknown <wandilemaluleke@gmail.com>
2022-01-24 13:52:36 +0200

    navbar center added

---
+++ login.js
@@ -0,0 +1,11 @@
+let names = [];
+let password = [];
+
+function addLogin() {
+    var names = document.getElementById("names");
+
+    if (names == "") {
+        alert("Email or Password is incorrect");
+        return;
+    }
+}
